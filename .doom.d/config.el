c6a6a4e2f41e51f0388fcfc8be0304afac7973d9
Aleksandr Zakharov <aleksandrzakharov91@gmail.com>
2022-08-24 12:43:53 +0500

    Adds project path

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -42,7 +42,7 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-
+(setq projectile-project-search-path '("~/dev/"))
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
